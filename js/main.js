3059bd6468fe79ee64ace42d909fb1b8c3171c02
IbrahimKhatibDev <ggbrhim@gmail.com>
2024-10-03 13:44:11 -0600

    started step 3

--- js/main.js
+++ js/main.js
@@ -1,1 +1,7 @@
 // Enter your code below.
+const resourcesDiv = document.querySelector('.javascript-resources');
+const showResourcesButton = document.getElementById('show-resources');
+
+showResourcesButton.addEventListener('click', () => { resourcesDiv.classList.remove('d-none')});
+
+const resourceList = resourcesDiv.querySelector('all');
