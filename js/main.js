8fe5d2eb3d56adae8d4a9713bf68f483b27b93f0
IbrahimKhatibDev <ggbrhim@gmail.com>
2024-10-03 14:16:20 -0600

    made some chages to the code for italics and bold

--- js/main.js
+++ js/main.js
@@ -1,7 +1,27 @@
 // Enter your code below.
+// Step 1
 const resourcesDiv = document.querySelector('.javascript-resources');
 const showResourcesButton = document.getElementById('show-resources');
 
-showResourcesButton.addEventListener('click', () => { resourcesDiv.classList.remove('d-none')});
+// Step 2
+showResourcesButton.addEventListener('click', () => {
+     resourcesDiv.classList.remove('d-none')
+});
 
-const resourceList = resourcesDiv.querySelector('all');
+// Step 3
+// when hovering bolds the letters
+resourcesDiv.addEventListener('mouseover', function (event) {
+    console.log(event.target);
+
+    event.target.classList.contains('list-group-item') && event.target.classList.add('fw-bold');
+});
+
+// when no longer hovering goes back to normal letters
+resourcesDiv.addEventListener('mouseout', function (event) {
+    event.target.classList.contains('list-group-item') && event.target.classList.remove('fw-bold');
+});
+
+// when clicked becomes italic
+resourcesDiv.addEventListener('click', function (event) {
+    event.target.classList.contains('list-group-item') && event.target.classList.add('fst-italic');
+});
